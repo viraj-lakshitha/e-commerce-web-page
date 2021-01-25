546fe80474b5ef51bd2fa1ede935a846888931a0
viraj-lakshitha <virajlakshithabandara@gmail.com>
2021-01-25 13:28:56 +0530

    Navigation Updated
    Use SVG in Folder, Don't Use FontAwesome Now, Final Stage we can change

--- assests/js/index.js
+++ assests/js/index.js
@@ -1,4 +1,23 @@
 /* Navigation */
+const navOpen = document.querySelector(".nav__hamburger");
+const navClose = document.querySelector(".close__toggle");
+const menu = document.querySelector(".nav__menu");
+const scrollLink = document.querySelectorAll(".scroll-link");
+const navContainer = document.querySelector(".nav__menu");
+
+navOpen.addEventListener("click", () => {
+  menu.classList.add("open");
+  document.body.classList.add("active");
+  navContainer.style.left = "0";
+  navContainer.style.width = "30rem";
+});
+
+navClose.addEventListener("click", () => {
+  menu.classList.remove("open");
+  document.body.classList.remove("active");
+  navContainer.style.left = "-30rem";
+  navContainer.style.width = "0";
+});
 
 /* Pop-Up */
 
